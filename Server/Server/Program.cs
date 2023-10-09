244378a90656291637af5252d0e80921e7a57785
Bubomira <106514492+Bubomira@users.noreply.github.com>
2023-10-09 17:00:25 +0300

    database connection included

--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Server.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<LearnMeDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultCoonection"));
+});
+
 
 var app = builder.Build();
 
