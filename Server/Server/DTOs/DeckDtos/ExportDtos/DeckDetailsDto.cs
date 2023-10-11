ecb97e20e50fc54dc0f27b301c2f92a4a7861e5d
Bubomira <106514492+Bubomira@users.noreply.github.com>
2023-10-11 18:14:44 +0300

    new dtos

---
+++ Server/Server/DTOs/DeckDtos/ExportDtos/DeckDetailsDto.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Server.DTOs.DeckDtos.ExportDtos
+{
+    public class DeckDetailsDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int OwnerId { get; set; }
+
+        public ICollection<string> Tags { get; set; }
+        public ICollection<string> Flashcards { get; set; }
+
+    }
+}
---
+++ Server/Server/DTOs/FlashcardDtos/ExportDtos/FlashcardBasicDto.cs
@@ -0,0 +1,11 @@
+﻿using Server.Enums;
+
+namespace Server.DTOs.FlashcardDtos.ExportDtos
+{
+    public class FlashcardBasicDto
+    {
+        public int Id { get; set; }
+        public FlashcardType Type { get; set; }
+        public string Definition { get; set; }
+    }
+}
