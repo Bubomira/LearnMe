032ae99494fffa4b13812fbb62c2a3b2a924969f
Bubomira <106514492+Bubomira@users.noreply.github.com>
2023-10-11 16:33:29 +0300

    flashcard dtos

---
+++ Server/Server/DTOs/FlashcardDtos/ExportDtos/FlashcardDetailsDto.cs
@@ -0,0 +1,17 @@
+﻿using Server.Enums;
+
+namespace Server.DTOs.FlashcardDtos.ExportDtos
+{
+    public class FlashcardDetailsDto
+    {
+        public int Id { get; set; }
+        public FlashcardType Type { get; set; }
+
+        public string Definition { get; set; }
+
+        public string Explanation { get; set; }
+
+        public ICollection<string> Tags { get; set; }
+        public int OwnerId { get; set; }
+    }
+}
---
+++ Server/Server/DTOs/FlashcardDtos/ImportDtos/FlashcardInfoDto.cs
@@ -0,0 +1,15 @@
+﻿using Server.Enums;
+
+namespace Server.DTOs.FlashcardDtos.ImportDtos
+{
+    public class FlashcardInfoDto
+    {
+        public string Type { get; set; }
+
+        //either picture or text, depending on the type
+        public string Definition { get; set; }
+
+        public string Explanation { get; set; }
+
+    }
+}
