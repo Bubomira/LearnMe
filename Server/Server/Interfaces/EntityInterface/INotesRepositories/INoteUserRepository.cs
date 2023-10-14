9b3ae646d3b08fad9fd7cebac4e4e79bb3549442
Bubomira <106514492+Bubomira@users.noreply.github.com>
2023-10-14 20:42:51 +0300

    user like notes repository

---
+++ Server/Server/Interfaces/EntityInterface/INotesRepositories/INoteUserRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Server.Interfaces.EntityInterface.INotesRepositories
+{
+    public interface INoteUserRepository
+    {
+        public Task<bool> CheckIfNoteIsLikedByUser(int noteId, int userId);
+        public Task LikeNote(int noteId, int userId);
+        public Task DislikeNote(int noteId, int userId);
+    }
+}
---
+++ Server/Server/Repositories/EntityRepositories/NotesRepositories/NoteUserRepository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Server.Data;
+using Server.Interfaces.EntityInterface.INotesRepositories;
+using Server.Models;
+
+namespace Server.Repositories.EntityRepositories.NotesRepositories
+{
+    public class NoteUserRepository : INoteUserRepository
+    {
+        private readonly LearnMeDbContext _learnMeDbContext;
+
+        public NoteUserRepository(LearnMeDbContext learnMeDbContext)
+        {
+            _learnMeDbContext = learnMeDbContext;
+        }
+        public Task<bool> CheckIfNoteIsLikedByUser(int noteId, int userId)
+            => _learnMeDbContext.LikedNotesUsers.AnyAsync(lnu => lnu.NoteId == noteId && lnu.LikerUserId == userId);
+
+
+        public async Task DislikeNote(int noteId, int userId)
+        {
+            var noteUser = await _learnMeDbContext.LikedNotesUsers
+                  .FirstOrDefaultAsync(lnu => lnu.LikerUserId == userId && lnu.NoteId == noteId);
+
+            _learnMeDbContext.LikedNotesUsers.Remove(noteUser);
+
+            await _learnMeDbContext.SaveChangesAsync();
+        }
+
+        public async Task LikeNote(int noteId, int userId)
+        {
+            NoteUser noteUser = new NoteUser()
+            {
+                NoteId = noteId,
+                LikerUserId = userId
+            };
+
+            await _learnMeDbContext.LikedNotesUsers.AddAsync(noteUser);
+
+            await _learnMeDbContext.SaveChangesAsync();
+        }
+    }
+}
