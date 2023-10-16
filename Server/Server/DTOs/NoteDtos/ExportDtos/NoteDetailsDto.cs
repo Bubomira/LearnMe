f1db0c6da89a007f946a7d83f585a73c5146a512
Bubomira <106514492+Bubomira@users.noreply.github.com>
2023-10-16 19:43:01 +0300

    note dtos

---
+++ Server/Server/DTOs/NoteDtos/ExportDtos/NoteDetailsDto.cs
@@ -0,0 +1,13 @@
+﻿using Server.Models;
+
+namespace Server.DTOs.NoteDtos.ExportDtos
+{
+    public class NoteDetailsDto
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public int OwnerId { get; set; }
+        public string[] Tags { get; set; }
+
+    }
+}
---
+++ Server/Server/DTOs/NoteDtos/ImportDtos/NoteInfoDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Server.DTOs.NoteDtos.ImportDtos
+{
+    public class NoteInfoDto
+    {
+        public string Content { get; set; }
+        public string[] Tags { get; set; }
+    }
+}
