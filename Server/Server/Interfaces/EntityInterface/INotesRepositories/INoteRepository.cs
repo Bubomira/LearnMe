eb964c9584eba1b8fbce14df3da8407c65781c8d
Bubomira <106514492+Bubomira@users.noreply.github.com>
2023-10-16 18:47:03 +0300

    note repository + get/delete note and 2 checkers

---
+++ Server/Server/Interfaces/EntityInterface/INotesRepositories/INoteRepository.cs
@@ -0,0 +1,15 @@
+﻿using Server.Models;
+
+namespace Server.Interfaces.EntityInterface.INotesRepositories
+{
+    public interface INoteRepository
+    {
+        public Task<Note> GetNotesDetails(int noteId);
+        public Task<Note> CreateNote();
+        public Task UpdateNote(int noteId);
+        public Task DeleteNote(int noteId);
+        public Task<bool> CheckIfNoteExists(int noteId);
+        public Task<bool> CheckIfNoteIsOwnedByUser(int noteId, int userId);
+
+    }
+}
---
+++ Server/Server/Repositories/EntityRepositories/NotesRepositories/NoteRepository.cs
@@ -0,0 +1,37 @@
+﻿using Server.Interfaces.EntityInterface.INotesRepositories;
+using Server.Models;
+
+namespace Server.Repositories.EntityRepositories.NotesRepositories
+{
+    public class NoteRepository : INoteRepository
+    {
+        public Task<bool> CheckIfNoteExists(int noteId)=>
+
+       
+
+        public Task<bool> CheckIfNoteIsOwnedByUser(int noteId, int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Note> CreateNote()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteNote(int noteId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Note> GetNotesDetails(int noteId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateNote(int noteId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
