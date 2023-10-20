839cac517ec71e9d0978bd6dc6f08e4abd1aee55
Bubomira <106514492+Bubomira@users.noreply.github.com>
2023-10-20 19:42:05 +0300

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
+import { Route,Routes } from "react-router-dom";
 import Navigation from "./components/navigationComponents/Navigation";
+import Login from "./components/authComponents/LoginComponent/Login";
 
 
 function App() {
   return (
+    <>
     <Navigation/>
+       <Routes>
+         <Route path="/login" element={<Login/>}/>
+       </Routes>
+    </>
   );
 }
 
