67c11e4c1bb1818fcad3d9e5a3bb599a170c8bb6
Bubomira <106514492+Bubomira@users.noreply.github.com>
2023-10-22 12:32:51 +0300

    authContext

---
+++ client/src/contexts/AuthContext.js
@@ -0,0 +1,23 @@
+import { createContext } from "react";
+
+import useLokalStorageAuth from '../hooks/useLokalStorageAuth'
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({children})=>{
+    let [user,setUser] =useLokalStorageAuth({});
+
+    const loginUser = (newUser)=>{
+        setUser(newUser);
+    }
+
+    const logoutUser =()=>{
+        setUser({});
+    }
+
+    return (
+    <AuthContext.Provider  value={{user:user, loginUser, logoutUser }}>
+         {children}
+    </AuthContext.Provider>
+    )
+}
