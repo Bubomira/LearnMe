1e0e011ba39c97a6447bbaccd115f0e0bdfa9cd9
Bubomira <106514492+Bubomira@users.noreply.github.com>
2023-10-25 15:11:06 +0300

    deck crud methods service

---
+++ client/src/services/deckServices.js
@@ -0,0 +1,11 @@
+import {get, post,put,del } from "../utils/requester";
+
+const endpointUrl = 'https://localhost:7065/api/deck';
+
+export const getDeck =(deckId)=> get(`${endpointUrl}/details/${deckId}`);
+
+export const createDeck = (data)=>post(`${endpointUrl}/create`,data);
+
+export const updateDeck = (deckId,data)=>put(`${endpointUrl}/update/${deckId}`,data)
+
+export const deleteDeck =(deckId)=>del(`${endpointUrl}/delete/${deckId}`)
