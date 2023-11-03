888520c6b9a5118343e9a924b7a93b22e0021ff7
Bubomira <106514492+Bubomira@users.noreply.github.com>
2023-11-03 22:31:51 +0200

    flashcard details owner buttons + design

--- client/src/components/entityComponents/detailComponentPages/DeckDetailsComponent/FlashcardComponents/FlashcardDetailsComponent/FlashcardDetails.js
+++ client/src/components/entityComponents/detailComponentPages/DeckDetailsComponent/FlashcardComponents/FlashcardDetailsComponent/FlashcardDetails.js
@@ -1,16 +1,24 @@
 import './FlashcardDetails.css'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleLeft,faAngleRight } from '@fortawesome/free-solid-svg-icons'
+
 import { useContext,useEffect,useState } from 'react'
 
 import { useParams } from 'react-router-dom'
 
 import { DeckContext } from '../../../../../../contexts/entityContexts/DeckContext'
+import { AuthContext } from '../../../../../../contexts/AuthContext'
 
 import { getFlashcardDetails } from '../../../../../../services/entityService/flashcardServices'
 
+import OwnerButtons from '../../../../ButtonComponents/OwnerButtonsComponent/OwnerButtons'
+
 export default function FlashcardDetails(){
     const {deckId,flashcardId} = useParams();
+
     const {deck} = useContext(DeckContext);
+    const {user} = useContext(AuthContext);
 
     const [flashcard,setFlashcard] = useState({})
 
@@ -27,9 +35,18 @@ export default function FlashcardDetails(){
     },[flashcardId])
 
    return(
-    <article className='flashcard-details' onClick={clickerHandler}>
-        <h2>{clicked? flashcard.definition : flashcard.explanation}</h2>
-    </article>
-    
+    <section className='flashcard-details-wrapper'>
+        <p><FontAwesomeIcon icon={faAngleLeft}/></p> 
+        <article className='flashcard-details' onClick={clickerHandler}>
+            <h2>{clicked? flashcard.definition : flashcard.explanation}</h2>
+            {
+                flashcard.ownerId==user.Id?
+                <OwnerButtons entityType={'flashcard'} entityId={flashcardId}/>
+                :
+                <></>
+            }
+        </article>
+        <p><FontAwesomeIcon icon={faAngleRight}/></p>
+    </section>
    )
 }
