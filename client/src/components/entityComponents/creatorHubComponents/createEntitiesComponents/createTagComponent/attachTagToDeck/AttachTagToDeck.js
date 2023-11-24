662fdd403b33784dab3d493987ffc853a374024b
Bubomira <106514492+Bubomira@users.noreply.github.com>
2023-11-24 23:03:15 +0200

    useEffect for attach deck for url safety

--- client/src/components/entityComponents/creatorHubComponents/createEntitiesComponents/createTagComponent/attachTagToDeck/AttachTagToDeck.js
+++ client/src/components/entityComponents/creatorHubComponents/createEntitiesComponents/createTagComponent/attachTagToDeck/AttachTagToDeck.js
@@ -2,7 +2,13 @@ import CreateTag from "../CreateTag";
 
 import { useNavigate,useParams } from "react-router-dom";
 
+import { useEffect,useContext } from "react";
+
+import { DeckContext } from "../../../../../../contexts/entityContexts/DeckContext";
+
 import { attachTagToDeck } from "../../../../../../services/entityService/deckService/deckAditionalService";
+
+import { getDeck } from "../../../../../../services/entityService/deckService/deckServices";
 
 export default function AttachTagToDeck(){
 
@@ -10,9 +16,20 @@ export default function AttachTagToDeck(){
 
     const {deckId} = useParams();
 
+    const {deck,setDeckDetailed} = useContext(DeckContext)
+
+    useEffect(()=>{
+        getDeck(deckId).then(deck=>{
+            setDeckDetailed(deck)
+        }).catch(err=>{
+            navigate('/404')
+        })
+
+    },[deckId])
+
     const onAttachTagHandler = (e,tagName)=>{
         e.preventDefault();
-        attachTagToDeck(deckId,tagName).then(()=>{
+        attachTagToDeck(deck.id,tagName).then(()=>{
             navigate(`/deck/${deckId}`)
         }).catch(()=>{
             navigate('/404')
