a7338ee011336a5bf418578e7b722fd77a3c2006
Bubomira <106514492+Bubomira@users.noreply.github.com>
2023-11-30 21:57:12 +0200

    diagram context

---
+++ client/src/contexts/DiagramContext.js
@@ -0,0 +1,33 @@
+import { createContext,useState } from "react";
+
+import {Diagram,ShapeNode} from '@mindfusion/diagramming'
+
+
+export const DiagramContext = createContext();
+
+export const DiagramProvider = ({children})=>{
+
+    var shapeNames  = ['Actor','Alternative','Arrow3','Arrow5','Arrow8','Arrow9','Cloud','ConeUp','Cube','Cyllinder','']
+    let [diagram,setDiagram] = useState(new Diagram());
+
+    let [nodes,setNodes] = useState([])
+
+    const setNodesDiagram =()=>{
+        const shapes = [];
+        for (let index = 0; index < shapeNames.length; index++) {
+           const shapeName = shapeNames[index]
+           var node =new ShapeNode(diagram);
+           node.shape=shapeName
+           node.id=shapeName
+           node.text=shapeName
+           shapes.push(node)
+        }
+         setNodes(shapes)  
+    }
+
+    return (
+    <DiagramContext.Provider  value={{diagram:diagram,nodes:nodes,setNodesDiagram,shapeNames}}>
+         {children}
+    </DiagramContext.Provider>
+    )
+}
