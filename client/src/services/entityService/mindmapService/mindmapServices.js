293466b0fc20df93529ae56d82e108453dd70e94
Bubomira <106514492+Bubomira@users.noreply.github.com>
2023-11-30 21:56:46 +0200

    mindmap crud service

--- client/src/services/entityService/mindmapService/mindmapServices.js
+++ client/src/services/entityService/mindmapService/mindmapServices.js
@@ -2,4 +2,10 @@ import {get,post,put,del} from "../../../utils/requester";
 
 const endpointUrl = 'https://localhost:7065/api/mindmap';
 
+export const getMindmapDetails =(mindmapId)=>get(`${endpointUrl}/details/${mindmapId}`)
+
 export const createMindmap = (data)=> post(`${endpointUrl}/create`,data);
+
+export const updateMindmap =(mindmapId,data)=>put(`${endpointUrl}/update/${mindmapId}`,data)
+
+export const deleteMindmap = (mindmapId)=> del(`${endpointUrl}/delete/${mindmapId}`);
