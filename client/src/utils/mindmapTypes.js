9fe12420f482f8b008da223b28458bb95a5e84fb
Bubomira <106514492+Bubomira@users.noreply.github.com>
2023-12-02 22:15:50 +0200

    mindmap types

---
+++ client/src/utils/mindmapTypes.js
@@ -0,0 +1,11 @@
+import {MindmapNode} from '../components/entityComponents/detailComponentPages/MindmapDetailsComponent/MindmapNode/MindmapNode';
+import MindmapEdge from '../components/entityComponents/detailComponentPages/MindmapDetailsComponent/MindmapEdge/MindmapEdge';
+ 
+ 
+export const nodeTypes = {
+  mindmap: MindmapNode,
+};
+ 
+export const edgeTypes = {
+  mindmap: MindmapEdge,
+};
