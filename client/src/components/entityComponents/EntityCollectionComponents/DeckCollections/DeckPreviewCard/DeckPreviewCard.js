055318379edb0631e80418c9550fd4508e974873
Bubomira <106514492+Bubomira@users.noreply.github.com>
2023-12-05 20:35:43 +0200

    deck preview card

---
+++ client/src/components/entityComponents/EntityCollectionComponents/DeckCollections/DeckPreviewCard/DeckPreviewCard.js
@@ -0,0 +1,24 @@
+import './DeckPreviewCard.css'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import { faRectangleList } from '@fortawesome/free-solid-svg-icons'
+
+import { Link } from "react-router-dom"
+
+export default function DeckPreviewCard({deck}){
+
+    return(
+         <article className="deck-preview-wrapper">
+            <div className='deck-preview-info-holder'>
+                 <FontAwesomeIcon fontSize={'3em'} icon={faRectangleList}/>
+               <h3>{deck.name}</h3>
+            </div>
+             <div className="deck-preview-tags">
+                {deck.tags.map(tag=><p id={tag.id}>{tag.name}</p>)}
+             </div>
+             <button><Link to={`/deck/${deck.id}`}>Details</Link></button>
+         </article>
+    )
+
+}
