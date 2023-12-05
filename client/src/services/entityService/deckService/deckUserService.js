2a72b98325bdb9a2554eab0a0b193112c24e8de5
Bubomira <106514492+Bubomira@users.noreply.github.com>
2023-12-05 19:32:38 +0200

    get liked/owned decks service

--- client/src/services/entityService/deckService/deckUserService.js
+++ client/src/services/entityService/deckService/deckUserService.js
@@ -5,3 +5,7 @@ const endpointUrl = 'https://localhost:7065/api/deck/user';
 export const likeDeck =(deckId)=>get(`${endpointUrl}/like/deck/${deckId}`);
 
 export const dislikeDeck = (deckId)=>get(`${endpointUrl}/dislike/deck/${deckId}`)
+
+export const getOwnedDecks = ()=>get(`${endpointUrl}/get/ownedDecks`)
+
+export const getLikedDecks = ()=>get(`${endpointUrl}/get/likedDecks`)
