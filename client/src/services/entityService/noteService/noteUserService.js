59064a18a9c217af4439d241e1d099e522bed08a
Bubomira <106514492+Bubomira@users.noreply.github.com>
2023-12-06 19:06:04 +0200

    get liked and owned notes

--- client/src/services/entityService/noteService/noteUserService.js
+++ client/src/services/entityService/noteService/noteUserService.js
@@ -5,3 +5,7 @@ const endpointUrl = 'https://localhost:7065/api/note/user';
 export const likeNote =(noteId)=> get(`${endpointUrl}/like/${noteId}`)
 
 export const dislikeNote = (noteId)=> get(`${endpointUrl}/dislike/${noteId}`)
+
+export const getOwnedNotes = ()=>get(`${endpointUrl}/owned/notes`)
+
+export const getLikedNotes = ()=>get(`${endpointUrl}/liked/notes`)
