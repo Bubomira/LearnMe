33b3a94d926f73aeef19b1b2a46f907c520939be
Bubomira <106514492+Bubomira@users.noreply.github.com>
2023-12-08 15:07:01 +0200

    get liked and owned mindmaps

--- client/src/services/entityService/mindmapService/mindmapUserService.js
+++ client/src/services/entityService/mindmapService/mindmapUserService.js
@@ -5,3 +5,7 @@ const endpointUrl = 'https://localhost:7065/api/mindmap/user';
 export const likeMindmap = (mindmapId) =>get(`${endpointUrl}/like/mindmap/${mindmapId}`);
 
 export const dislikeMindmap =(mindmapId) =>get(`${endpointUrl}/dislike/mindmap/${mindmapId}`);
+
+export const getLikedMindmaps =()=>get(`${endpointUrl}/get/liked`)
+
+export const getOwnedMindmaps =()=>get(`${endpointUrl}/get/owned`)
