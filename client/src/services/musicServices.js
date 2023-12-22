bdc4ab2bfc8bc01d0dc6f63bc7080f013b4d5779
Bubomira <106514492+Bubomira@users.noreply.github.com>
2023-12-22 15:42:14 +0200

    get playlist details

--- client/src/services/musicServices.js
+++ client/src/services/musicServices.js
@@ -4,7 +4,7 @@ const client_secret = '494ebd99d0ec4fe9b1af4bee2e4e44b9';
 const user_id = `31i3kbxrx4q5ygd7t6uyxzrz65dq`
 const authUrl = 'https://accounts.spotify.com'
 
-const baseUrl='https://api.spotify.com/v1/users'
+const baseUrl='https://api.spotify.com/v1'
 
 export const getAccessToken=async()=>{
 
@@ -24,7 +24,20 @@ export const getAccessToken=async()=>{
 export const getPlaylists = async()=>{
     const musicToken = JSON.parse(localStorage.getItem('musicToken')).access_token;
 
-    const res  = await fetch(`${baseUrl}/${user_id}/playlists`,{
+    const res  = await fetch(`${baseUrl}/users/${user_id}/playlists`,{
+        headers:{
+            'Authorization':`Bearer ${musicToken}`
+        }
+    })
+    if(res.ok){
+        return res.json();
+    }
+}
+
+export const getPlaylistDetails = async(playlistId)=>{
+    const musicToken = JSON.parse(localStorage.getItem('musicToken')).access_token;
+
+    const res  = await fetch(`${baseUrl}/playlists/${playlistId}`,{
         headers:{
             'Authorization':`Bearer ${musicToken}`
         }
