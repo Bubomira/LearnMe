f4de7894c15a7bc6f4af796d5ac43ac9e4e72ff7
Bubomira <106514492+Bubomira@users.noreply.github.com>
2023-12-29 23:07:03 +0200

    agenda license

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
+import { registerLicense } from '@syncfusion/ej2-base';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NAaF5cWWJCf0x0TXxbf1x0ZFdMYFRbRX9PIiBoS35RdURhW3ZfdHRdR2lUV012');
 root.render(
   <BrowserRouter>
     <App />
