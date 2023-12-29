0d58daf3892e3bab4d3a836e96692b7621cfe871
Bubomira <106514492+Bubomira@users.noreply.github.com>
2023-12-29 20:13:42 +0200

    new deploy server

---
+++ Server/Server/Migrations/20231229174612_nc.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class nc : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EventsJSON",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EventsJSON",
+                table: "Users");
+        }
+    }
+}
