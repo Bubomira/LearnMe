2207294db56e35e967ff4c513fb947577500d538
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-01-14 13:07:22 +0200

    note collection css improved

--- client/src/components/entityComponents/PreviewCardComponent/NotePreviewCard/NotePreviewCard.js
+++ client/src/components/entityComponents/PreviewCardComponent/NotePreviewCard/NotePreviewCard.js
@@ -4,28 +4,28 @@ import { Link } from 'react-router-dom'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-import { faStickyNote } from '@fortawesome/free-solid-svg-icons'
+import { faNoteSticky } from '@fortawesome/free-solid-svg-icons'
+
 
 export default function NotePreviewCard({note}){
 
     return(
         <article className="note-preview-wrapper">
-            <main>
+            <main className='note-preview-main'>
                 <h2 className='note-preview-header'>
                     {note.title}
                  </h2>
-                 <section className="note-preview-tags">
                     {note?.tags?
-                      note.tags.map(tag=><p key={tag.id}>{tag.name}</p>):
+                      <p className='tags'>{note?.tags.map(tag=>tag.name).join(', ')}</p>
+                      :
                       <></>
                     }
-                 </section>
                  <button className="note-preview-details">
                     <Link to={`/note/${note.id}`}>Details</Link>
                  </button>
             </main>
             <aside className='note-preview-aside'>
-                <FontAwesomeIcon icon={faStickyNote}/>
+                <FontAwesomeIcon color='white' fontSize={'2em'} icon={faNoteSticky}/>
             </aside>
         </article>
     )
