8a8b9c0c63dc28b9bf82e0c7cf13713e15aebd00
Bubomira <106514492+Bubomira@users.noreply.github.com>
2024-01-14 12:27:20 +0200

    photo recogniser with bulgarian

--- client/src/utils/recognise.js
+++ client/src/utils/recognise.js
@@ -1,7 +1,7 @@
 import { createWorker } from "tesseract.js";
 
 export const recognise = async(ImageUrl)=>{
-    const worker =await createWorker('eng');
+    const worker =await createWorker('eng+bul');
     const ret = await worker.recognize(ImageUrl)
     await worker.terminate();
     return ret.data.text;
